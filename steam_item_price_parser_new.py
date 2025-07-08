62323657d46685384450c2743ab90cdbc4a8aeaf
Артем <artem.kubasov.2016@gmail.com>
2025-07-08 03:10:38 +0300

    использовал класс, пока думаю

--- steam_item_price_parser_new.py
+++ steam_item_price_parser_new.py
@@ -1,23 +1,27 @@
-from locale import resetlocale
+#
+
+import time
+import random
+import pandas as pd
+import openpyxl as OP
+import phantomjs
+import bs4
+import pathlib
 
 #
 
+from importlib.metadata import requires
+from pathlib import Path
+from locale import resetlocale
+from bs4 import BeautifulSoup
+from openpyxl import load_workbook
+from openpyxl import Workbook
 from selenium.webdriver import Edge
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-#
-
-from bs4 import BeautifulSoup
-import pandas as pd
-import openpyxl as OP
-from openpyxl import load_workbook
-from openpyxl import Workbook
-import time
-import random
 
 #
 
@@ -31,6 +35,7 @@ options.add_argument( "--guest" )
 if __name__ == "__main__":
     service = Service( executable_path="./msedgedriver.exe", log_path='NUL' )
     driver = Edge( service=service, options=options )  
+
 
 #=====#=====#=====#=====#=====#=====#=====#=====#=====#=====#=====#=====#=====#=====#=====#=====#=====#=====#=====#=====#=====#=====#=====#=====# settings | options
 
@@ -52,45 +57,49 @@ rates = [
     { "name": "USD/RUB", "url": "https://cbr.ru", "bfr": "A1"}
     ]
 
-def find_USD_to_RUB ( rates ):
-
-    driver.get ( rates[0]["url"] )
-    time.sleep ( 3 )
-    html_source = driver.page_source
-    soup = BeautifulSoup( html_source, 'html.parser' )
-
-    usd_rates_pre = soup.find_all( 'div', class_='col-md-2 col-xs-9 _right mono-num' )
-    usd_rate_pre = usd_rates_pre[ 3 ]
-    usd_rate = usd_rate_pre.text.strip()
-
-    return usd_rate
 
 
-def find_price (items ):
+class Parsers_with_BS_selenium:
 
-    for item in items:
-
-        driver.get( item["url"] )
-        time.sleep( 5 )
-        html_source = driver.page_source
-        soup = BeautifulSoup( html_source, 'html.parser' )
-
-        prices = soup.find_all( 'span', class_='market_commodity_orders_header_promote' )
-        price = prices[ 1 ].text.strip()
-        prices_final.append( price )
-
-    return prices_final
+    """This is a documentation for the Parsers class.
+This is a class that represents parsers with an attribute of object to parse, prices or rates."""
 
 
-usd_rate = find_USD_to_RUB ( rates )
-find_price ( items )
+    def __init_( self, purpose, driver ):
+        self.purpose = purpose
+        self.driver = driver
+
+    def usd_rub_parser( self ):
+        for currency in rates:
+            self.driver.get( currency[ "url" ] )
+            time.sleep( 5 )
+            html_source = self.driver.page_source
+            soup = BeautifulSoup( html_source, 'html.parser' )
+
+            currency_rate = soup.find_all( 'div', class_='col-md-2 col-xs-9 _right mono-num' )
+            currency_rate = currency_rate [ 3 ]
+            currency_rate = currency_rate.text.strip()
+
+        return currency_rate
 
 
-for price in prices_final:
-    print ( price )
+    def items_prices_parser( self ):
+        for item in items:
+            self.driver.get ( item[ "url" ] )
+            time.sleep(5)
+            html_source = self.driver.page_source
+            soup = BeautifulSoup( html_source, 'html.parser' )
 
-print ( usd_rate )
+            item_price = soup.find_all( 'span', class_='market_commodity_orders_header_promote' )
+            item_price = item_price[ 1 ]
+            item_price = item_price.text.strip()
+            prices_final.append(item_price)
+        
+        return None
 
-input( "Press Enter to close the window" )
-driver.quit ()
 
+rate = Parsers_with_BS_selenium.usd_rub_parser
+Parsers_with_BS_selenium.items_prices_parser
+
+print (Parsers_with_BS_selenium.__doc__, "\n")
+print ("Rate for USD|RUB : ", rate, "\n", "\n", "Prices of items : ", prices_final)
